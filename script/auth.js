eecd23758c653e3af645f29d12017e173a4513eb
Devindi-u <panchalidevindi@gmail.com>
2025-08-17 10:52:52 +0530

    signup and login js part

--- script/auth.js
+++ script/auth.js
@@ -10,3 +10,92 @@ const firebaseConfig = {
     measurementId: "G-M22SCG7FP4"
 };
 
+//initializing firebase
+firebaseConfig.initializeApp(firebaseConfig);
+const auth = firebaseConfig.auth();
+const database = firebaseConfig.database();
+
+//DOM Elements
+const authContainer = document.getElementById('auth-container');
+const loginForm = document.querySelector('.auth-form:first-of-type');
+const signupForm = document.querySelector('.auth-form:last-of-type');
+const showSignup = document.getElementById('show-signup');
+const showLogin = document.getElementById('show-login');
+const loginBtn = document.getElementById('login-btn');
+const signupBtn = document.getElementById('signup-btn');
+const logoutBtn = document.getElementById('logout-btn');
+
+showSignup.addEventListener('click', () => {
+    loginForm.style.display = 'none';
+    setTimeout(() => {
+        loginForm.classList.add('hidden');
+        signupForm.classList.remove('hidden');
+        signupForm.style.display = 'block';
+    },10);
+});
+
+showLogin.addEventListener('click', () => {
+    signupForm.style.display = 'none';
+    setTimeout(() => {
+        signupForm.classList.add('hidden');
+        loginForm.classList.remove('hidden');
+        loginForm.style.display = 'block';
+    },10);
+});
+
+//signup function
+signupBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('signup-name').value;
+    const email = document.getElementById('signup-email').value;
+    const password = document.getElementById('signup-password').value;
+
+    auth.createUserWithEmailAndPassword(email,password)
+    .then((userCredential) => {
+        //signed up
+        const user = userCredential.user;
+
+        //save additional
+        database.ref('users/' + user.uid).set({
+            name: name,
+            email: email
+        }).then(() => {
+            console.log('User data saved!');
+            authContainer.classList.add('hidden');
+            document.getElementById('signup-name').value = '';
+            document.getElementById('signup-email').value = '';
+            document.getElementById('signup-password').value = '';
+        });
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    })
+});
+
+//login function
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        //signed in
+        const user = userCredential.user;
+        console.log('User logged in: ', user);
+        authContainer.classList.add('hidden');
+        document.getElementById('login-email').value = '';
+        document.getElementById('login-password').value = '';
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    });
+});
+
+//logout function
