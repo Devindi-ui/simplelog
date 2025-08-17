8b6a03a9bf5984f338d56e7ec3ded483d34cad18
Devindi-u <panchalidevindi@gmail.com>
2025-08-17 09:36:07 +0530

    config firebase

---
+++ script/auth.js
@@ -0,0 +1,12 @@
+//firebase configuarations
+const firebaseConfig = {
+    apiKey: "AIzaSyAOkD60nkzwTKoZKv1t95bPzx_eUdR0CZQ",
+    authDomain: "simplelog-1430b.firebaseapp.com",
+    databaseURL: "https://simplelog-1430b-default-rtdb.firebaseio.com",
+    projectId: "simplelog-1430b",
+    storageBucket: "simplelog-1430b.firebasestorage.app",
+    messagingSenderId: "49007713019",
+    appId: "1:49007713019:web:26e4f9e15c4fc7e98cae5e",
+    measurementId: "G-M22SCG7FP4"
+};
+
